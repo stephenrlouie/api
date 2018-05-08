df5baf500b9e5213ca9fb9bedbad91b8348ac9a0
stephenrlouie <stelouie@cisco.com>
2018-05-08 15:33:43 -0400

    build push script

--- build-push-image.sh
+++ build-push-image.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-
-REPO = optikon/api
+SHORT_SHA=${TRAVIS_COMMIT} | awk '{print substr($0,0,7)}'
+REPO=optikon/api
 
 # If the tag is undefined
-if [-z $TRAVIS_TAG ]; then
+if [ -z $TRAVIS_TAG ]; then
   echo "NOT A TAG"
+  echo ${SHORT_SHA}
+  echo ${REPO}
   # docker push $REPO:$SHORT_SHA
   # docker tag $REPO:$SHORT_SHA $REPO:latest
   # docker push $REPO:latest
 # If the tag is set
 else
   echo "THIS IS A TAG"
+  echo ${SHORT_SHA}
+  echo ${REPO}
   docker tag $REPO:$SHORT_SHA $REPO:$TRAVIS_TAG
   # docker push $REPO:$TRAVIS_TAG
 fi
