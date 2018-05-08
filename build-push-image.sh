e2a09b1090a481492423c6695571733c3197d962
stephenrlouie <stephen.r.louie@gmail.com>
2018-05-08 16:48:29 -0400

    echo prints

--- build-push-image.sh
+++ build-push-image.sh
@@ -10,12 +10,15 @@ if [ ! -z $TRAVIS_TAG ]; then
   echo "Tag"
   docker tag $REPO:$SHORT_SHA $REPO:$TRAVIS_TAG
   docker push $REPO:$TRAVIS_TAG
+  echo "Docker pushed ${REPO}:${TRAVIS_TAG}"
 # If the tag is set
 elif [ "$TRAVIS_BRANCH" == "trav" ]; then
   echo "Master branch"
   docker push $REPO:$SHORT_SHA
+  echo "Docker pushed ${REPO}:${SHORT_SHA}"
   docker tag $REPO:$SHORT_SHA $REPO:latest
   docker push $REPO:latest
+  echo "Docker pushed ${REPO}:latest"
 else
   echo "No Op"
 fi
