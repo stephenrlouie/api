44267997ac8593b482c08c1157267b683b74c3a6
stephenrlouie <stephen.r.louie@gmail.com>
2018-05-08 15:51:07 -0400

    properly handling tag and master branch

--- build-push-image.sh
+++ build-push-image.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 SHORT_SHA=`echo ${TRAVIS_COMMIT::7}`
 REPO=optikon/api
-echo $TRAVIS_COMMIT
 
 # If the tag is undefined
-if [ -z $TRAVIS_TAG ]; then
-  echo "NOT A TAG"
-  echo ${SHORT_SHA}
-  echo ${REPO}
-  # docker push $REPO:$SHORT_SHA
-  # docker tag $REPO:$SHORT_SHA $REPO:latest
-  # docker push $REPO:latest
-# If the tag is set
-else
-  echo "THIS IS A TAG"
-  echo ${SHORT_SHA}
-  echo ${REPO}
+if [ ! -z $TRAVIS_TAG ]; then
   docker tag $REPO:$SHORT_SHA $REPO:$TRAVIS_TAG
-  # docker push $REPO:$TRAVIS_TAG
+  docker push $REPO:$TRAVIS_TAG
+# If the tag is set
+elif [ "$TRAVIS_BRANCH" == "master" ]
+  docker push $REPO:$SHORT_SHA
+  docker tag $REPO:$SHORT_SHA $REPO:latest
+  docker push $REPO:latest
 fi
