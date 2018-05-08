ba12a0f2f3eb43dcbaa5d15dce3042baee231113
stephenrlouie <stephen.r.louie@gmail.com>
2018-05-08 16:36:14 -0400

    echo prints for each workflow

--- build-push-image.sh
+++ build-push-image.sh
@@ -2,16 +2,19 @@
 SHORT_SHA=`echo ${TRAVIS_COMMIT::7}`
 REPO=optikon/api
 
-echo "${DOCKER_USER}"
 echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --password-stdin
 
 # If the tag is undefined
 if [ ! -z $TRAVIS_TAG ]; then
+  echo "Tag"
   docker tag $REPO:$SHORT_SHA $REPO:$TRAVIS_TAG
   docker push $REPO:$TRAVIS_TAG
 # If the tag is set
 elif [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "Master branch"
   docker push $REPO:$SHORT_SHA
   docker tag $REPO:$SHORT_SHA $REPO:latest
   docker push $REPO:latest
+else
+  echo "No Op"
 fi
