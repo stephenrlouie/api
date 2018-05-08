17cf15964178597697846c62a0a4f9e18010373f
stephenrlouie <stephen.r.louie@gmail.com>
2018-05-08 16:03:33 -0400

    attempts to push images

--- build-push-image.sh
+++ build-push-image.sh
@@ -2,12 +2,14 @@
 SHORT_SHA=`echo ${TRAVIS_COMMIT::7}`
 REPO=optikon/api
 
+docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+
 # If the tag is undefined
 if [ ! -z $TRAVIS_TAG ]; then
   docker tag $REPO:$SHORT_SHA $REPO:$TRAVIS_TAG
   docker push $REPO:$TRAVIS_TAG
 # If the tag is set
-elif [ "$TRAVIS_BRANCH" == "master" ]
+elif [ "$TRAVIS_BRANCH" == "master" ]; then
   docker push $REPO:$SHORT_SHA
   docker tag $REPO:$SHORT_SHA $REPO:latest
   docker push $REPO:latest
