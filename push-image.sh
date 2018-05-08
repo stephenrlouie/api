a84a2e71b0d2454c9c2b33d7aff9be6600e0cbeb
Steve Louie <stelouie@cisco.com>
2018-05-08 15:24:05 -0400

    shorten commit sha

--- push-image.sh
+++ push-image.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-env
+
+REPO = optikon/api
+
+# If the tag is undefined
+if [-z $TRAVIS_TAG ]; then
+  echo "NOT A TAG"
+  # docker push $REPO:$SHORT_SHA
+  # docker tag $REPO:$SHORT_SHA $REPO:latest
+  # docker push $REPO:latest
+# If the tag is set
+else
+  echo "THIS IS A TAG"
+  docker tag $REPO:$SHORT_SHA $REPO:$TRAVIS_TAG
+  # docker push $REPO:$TRAVIS_TAG
+fi
